edb6b8380d0088559d3e9e8b2177ddac954f6d4b
Sid Mani <11038851+sidmani@users.noreply.github.com>
2024-02-20 20:54:20 -0800

    move migration code

--- tsurara/datastore.py
+++ tsurara/datastore.py
@@ -1,6 +1,6 @@
 import json
-from .util import save_json
-from .migrations import apply_migrations
+from pathlib import Path
+from .util import save_json, WordState
 
 
 class Datastore:
@@ -33,4 +33,48 @@ class Datastore:
         return filename in self.data["seen_files"]
 
     def migrate(self):
-        return apply_migrations(self.data, self.path)
+        if "version" not in self.data:
+            init_version = 0
+            # migrate from old unique key to pure lemma
+            for group in {"seen", "ignore"}:
+                for k in list(self.data[group].keys()):
+                    del self.data[group][k]
+                    new_k = k.split(",")[0]
+                    self.data[group][new_k] = True
+
+            self.data["version"] = 1
+            self.save()
+        else:
+            init_version = self.data["version"]
+
+        if self.data["version"] == 1:
+            # migrate to version 2, which stores everything as one dictionary
+            new_dict = {}
+            for word in self.data["seen"]:
+                new_dict[word] = WordState.Seen
+
+            for word in self.data["ignore"]:
+                new_dict[word] = WordState.Ignore
+
+            self.data["words"] = new_dict
+            del self.data["seen"]
+            del self.data["ignore"]
+            self.data["version"] = 2
+            self.save()
+
+        if self.data["version"] == 2:
+            # add seen_files dict
+            self.data["seen_files"] = {}
+            self.data["version"] = 3
+            self.save()
+
+        if self.data["version"] == 3:
+            new_seen_files = {}
+            for p in self.data["seen_files"].keys():
+                new_seen_files[Path(p).name] = True
+
+            self.data["seen_files"] = new_seen_files
+            self.data["version"] = 4
+            self.save()
+
+        return (init_version, self.data["version"])
